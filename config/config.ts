5f3a5d6d4aa3a8f1c826393af1375b26c9f42f0e
sirhennig <andersonrmhennig@hotmail.com>
2021-12-24 19:00:18 -0300

    add readme

--- config/config.ts
+++ config/config.ts
@@ -1,11 +1,11 @@
 require('dotenv').config()
-const { DB_HOST, DB_USER, DB_PASS, DB_DLCT } = process.env;
+const { DB_HOST, DB_USER, DB_PASS, DB_DLCT, DB_NAME } = process.env;
 
 module.exports = {
     "development": {
         "username": DB_USER,
         "password": DB_PASS,
-        "database": "agenda",
+        "database": DB_NAME,
         "host": DB_HOST,
         "dialect": DB_DLCT
     },
